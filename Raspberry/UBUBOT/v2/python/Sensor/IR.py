41fb70201402f3f611b29b343c20d8112c390701
Roderick Diaz <about@rorik.me>
2019-02-22 18:22:07 +0100

    Fixed IR sensor initialization #7

--- Raspberry/UBUBOT/v2/python/Sensor/IR.py
+++ Raspberry/UBUBOT/v2/python/Sensor/IR.py
@@ -17,6 +17,7 @@ class IRSensor(object):
             GPIO.setmode(GPIO.BOARD)
             IRSensor._initialized_GPIO = True
         self._pin = pin
+        GPIO.setup(self._pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def wait(self, event, timeout=2000):
         return GPIO.wait_for_edge(self._pin, event.value, timeout=timeout) is not None
