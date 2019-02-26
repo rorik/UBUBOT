ff270a3c69abe3e71b2bfc1db650a7c4a8cb9fa7
Roderick Diaz <rdg1003@alu.ubu.es>
2019-02-26 21:06:21 +0100

    keyboard and square examples now use UBUBOT initializer #4

--- Raspberry/UBUBOT/v2/python/kb.py
+++ Raspberry/UBUBOT/v2/python/kb.py
@@ -1,48 +1,68 @@
 #!/usr/bin/python3
-import curses
-import os
-
 from Motor.MotorPair import MotorPair, MotorPairDirection, MotorIdentifier
+from util.Initializer import UBUBOT
+from argparse import ArgumentParser
+from sys import stdin, stdout
+import tty
+import termios
 
 
-def main(win):
-    speed=200
-    win.nodelay(True)
-    win.addstr("Direction = STOP")
-    while True:
-        try:
-            key = win.getkey()
-            if key == os.linesep:
-                break
-            elif key == 'w':
-                motors.run(MotorIdentifier.BOTH, speed)
-                direction = "Forwards"
-            elif key == 's':
-                motors.run(MotorIdentifier.BOTH, -speed)
-                direction = "Backwards"
-            elif key == 'a':
-                motors.turn_sharp(MotorPairDirection.SHARP_LEFT, speed, 90)
-                direction = "Sharp Left"
-            elif key == 'd':
-                motors.turn_sharp(MotorPairDirection.SHARP_RIGHT, speed, 90)
-                direction = "Sharp Right"
-            elif key == 'o':
-                motors.run(MotorIdentifier.RIGHT, speed)
-                direction = "Only Right Motor"
-            elif key == 'p':
-                motors.run(MotorIdentifier.LEFT, speed)
-                direction = "Only Left Motor"
-            else:
-                motors.stop()
-                direction = "STOP"
-            win.clear()
-            win.addstr("Direction = ")
-            win.addstr(direction)
-        except curses.error:
-            pass
-    motors.stop()
+def getChar():
+    fd = stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(stdin.fileno())
+        ch = stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
+def main(speed):
+    print("Press enter or 'p' to exit")
+    print("Direction = STOP")
+    key = getChar()
+    while not (key == '\r' or key == '\n' or key == 'p'):
+        if key == 'w':
+            ububot.motors.run(MotorIdentifier.BOTH, speed)
+            direction = "Forwards"
+        elif key == 's':
+            ububot.motors.run(MotorIdentifier.BOTH, -speed)
+            direction = "Backwards"
+        elif key == 'a':
+            ububot.motors.turn_sharp(MotorPairDirection.SHARP_LEFT, speed, 90)
+            direction = "Sharp Left"
+        elif key == 'd':
+            ububot.motors.turn_sharp(MotorPairDirection.SHARP_RIGHT, speed, 90)
+            direction = "Sharp Right"
+        elif key == 'q':
+            ububot.motors.move_by(MotorIdentifier.LEFT, speed=speed, angle=360)
+            direction = "Left"
+        elif key == 'e':
+            ububot.motors.move_by(MotorIdentifier.RIGHT, speed=speed, angle=360)
+            direction = "Right"
+        elif key == 'z':
+            ububot.motors.move_by(MotorIdentifier.LEFT, speed=speed, angle=-360)
+            direction = "Reverse Left"
+        elif key == 'e':
+            ububot.motors.move_by(MotorIdentifier.RIGHT, speed=speed, angle=-360)
+            direction = "Reverse Right"
+        else:
+            ububot.motors.stop()
+            direction = "STOP"
+        stdout.write("\033[F\033[K")
+        print("Direction =", direction)
+        key = getChar()
+    ububot.motors.stop()
 
 if __name__ == '__main__':
-    motors = MotorPair()
-    curses.wrapper(main)
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('-s', '--speed', type=int, default=200)
+    parser.add_argument('-sio', '--socket', dest='socket', action='store_const', const=True, default=False)
+    args = parser.parse_args()
+
+    ububot = UBUBOT(motors=True, serial_socket_capture=args.socket, motors_socket=args.socket)
+
+    try:
+        main(args.speed)
+    finally:
+        ububot.finalize()
--- Raspberry/UBUBOT/v2/python/square.py
+++ Raspberry/UBUBOT/v2/python/square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-from Motor.MotorPair import MotorPair, MotorPairDirection, MotorIdentifier
+from Motor.MotorPair import MotorIdentifier, MotorPairDirection
 from Sensor.IR import SensorEvent
-from util.Initializer import initialize
+from util.Initializer import UBUBOT
 from argparse import ArgumentParser
 from time import sleep
 
 if __name__ == '__main__':
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('N', type=int, default=4)
+    parser.add_argument('N', nargs='?', type=int, default=2)
     
-    motors, sensors = initialize()
+    ububot = UBUBOT(motors=True, sensors=True)
 
     for i in range(parser.parse_args().N * 4):
-        motors.run(MotorIdentifier.BOTH, speed=100)
-        sensors.get_north().wait(SensorEvent.DETECT_START, timeout=1000)
-        motors.advance_cm(-2, speed=50)
-        motors.turn_sharp(MotorPairDirection.SHARP_RIGHT, speed=100, angle=90)
+        ububot.motors.run(MotorIdentifier.BOTH, speed=100)
+        ububot.sensors.get_north().wait(SensorEvent.DETECT_START, timeout=1000)
+        ububot.motors.advance_cm(-5, speed=50)
         sleep(1)
+        ububot.motors.turn_sharp(MotorPairDirection.SHARP_RIGHT, speed=100, angle=90)
+        sleep(1)
+    
+    ububot.finalize()
 
 
