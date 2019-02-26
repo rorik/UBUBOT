d9630dacce3a1d4a04f8104a71bbcb542701c5cb
Roderick Diaz <rdg1003@alu.ubu.es>
2019-02-26 21:02:57 +0100

    Added result logger/capture for Serial and MotorPair #9

--- Raspberry/UBUBOT/v2/python/Comms/Serial.py
+++ Raspberry/UBUBOT/v2/python/Comms/Serial.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-import serial
+from time import sleep
+from threading import Thread
+from atexit import register
+from serial import Serial, PARITY_NONE, STOPBITS_ONE, EIGHTBITS
 
 class SerialCommunication:
     _connection = None
 
     def __init__(self):
         if SerialCommunication._connection is None:
-            SerialCommunication._connection = serial.Serial(
-                port='/dev/serial0',
-                baudrate=230400,
-                timeout=3,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS
+            SerialCommunication._connection = Serial(
+                    port='/dev/serial0',
+                    baudrate=230400,
+                    timeout=0,
+                    parity=PARITY_NONE,
+                    stopbits=STOPBITS_ONE,
+                    bytesize=EIGHTBITS
                 )
+            register(SerialCommunication.disconnect)
 
     def send(self, message, checksum=True, preview=False):
         data = "{{" + message
@@ -25,3 +29,41 @@ class SerialCommunication:
             print(data)
         
         SerialCommunication._connection.write(data.encode('ascii'))
+
+    def read_line(self):
+        return SerialCommunication._connection.read_until()
+
+    def flush_in(self):
+        SerialCommunication._connection.reset_input_buffer()
+
+    
+    @staticmethod
+    def disconnect():
+        if SerialCommunication._connection is not None:
+            SerialCommunication._connection.close()
+            SerialCommunication._connection = None
+
+
+class SerialCapture(SerialCommunication, Thread):
+
+    def start(self, output=lambda read: print(read, end='')):
+        Thread.__init__(self)
+        self.running = True
+        self.set_output(output)
+        Thread.start(self)
+    
+    def run(self):
+        line = ""
+        while (self.running and SerialCommunication._connection.is_open):
+            while (SerialCommunication._connection.in_waiting > 0):
+                line += self.read_line().decode('ascii')
+                if line[-1] == '\n':
+                    self.output(line)
+                    line = ""
+            sleep(0.05)
+    
+    def stop(self):
+        self.running = False
+    
+    def set_output(self, function):
+        self.output = function
--- Raspberry/UBUBOT/v2/python/Motor/MotorPair.py
+++ Raspberry/UBUBOT/v2/python/Motor/MotorPair.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 from Comms.Serial import SerialCommunication
 from enum import Enum
+from time import sleep
 
 
 class MotorPairDirection(Enum):
     SHARP_LEFT = 0
     SHARP_RIGHT = 1
+
 
 class MotorIdentifier(Enum):
     LEFT = 'L'
@@ -14,41 +16,65 @@ class MotorIdentifier(Enum):
 
 
 class MotorPair(object):
-    def __init__(self):
+    def __init__(self, result_listener=None):
         self._com = SerialCommunication()
+        self._result_listener = result_listener
 
     def stop(self):
         self._com.send("STOP")
-        
+        if self._result_listener is not None:
+            self._result_listener({"name": "stop"})
+
     def run(self, identifier, speed, time=None):
         if not isinstance(identifier, MotorIdentifier):
-            raise InvalidIdentifierError("Identifier must be of type MotorIdentifier")
+            raise InvalidIdentifierError(
+                "Identifier must be of type MotorIdentifier")
         if time is not None:
-            self._com.send("RNF;{};{};{}".format(identifier.value, speed, time))
+            self._com.send("RNF;{};{};{}".format(
+                identifier.value, speed, time))
+            if self._result_listener is not None:
+                self._result_listener({"name": "run_for", "identifier": identifier.value, "speed": speed, "time": time})
         else:
             self._com.send("RUN;{};{}".format(identifier.value, speed))
+            if self._result_listener is not None:
+                self._result_listener({"name": "run", "identifier": identifier.value, "speed": speed})
 
     def move_by(self, identifier, speed=200, angle=360):
         if not isinstance(identifier, MotorIdentifier):
-            raise InvalidIdentifierError("Identifier must be of type MotorIdentifier")
+            raise InvalidIdentifierError(
+                "Identifier must be of type MotorIdentifier")
         self._com.send("MOVB;{};{};{}".format(identifier.value, speed, angle))
+        if self._result_listener is not None:
+            self._result_listener({"name": "move_by", "identifier": identifier.value, "speed": speed, "angle": angle})
 
     def move_to(self, identifier, speed=200, angle=360):
         if not isinstance(identifier, MotorIdentifier):
-            raise InvalidIdentifierError("Identifier must be of type MotorIdentifier")
+            raise InvalidIdentifierError(
+                "Identifier must be of type MotorIdentifier")
         self._com.send("MOVT;{};{};{}".format(identifier.value, speed, angle))
+        if self._result_listener is not None:
+            self._result_listener({"name": "move_to", "identifier": identifier.value, "speed": speed, "angle": angle})
 
     def turn_sharp(self, direction, speed=200, angle=90):
         if direction is MotorPairDirection.SHARP_LEFT:
-            self._com.send("MOVB;{};{};{}".format(MotorIdentifier.LEFT.value, -speed, angle))
-            self._com.send("MOVB;{};{};{}".format(MotorIdentifier.RIGHT.value, speed, angle))
+            self._com.send("MOVB;{};{};{}".format(
+                MotorIdentifier.LEFT.value, speed, -angle))
+            self._com.send("MOVB;{};{};{}".format(
+                MotorIdentifier.RIGHT.value, speed, angle))
+            if self._result_listener is not None:
+                self._result_listener({"name": "turn_sharp", "direction": "left", "speed": speed, "angle": angle})
         elif direction is MotorPairDirection.SHARP_RIGHT:
-            self._com.send("MOVB;{};{};{}".format(MotorIdentifier.LEFT.value, speed, angle))
-            self._com.send("MOVB;{};{};{}".format(MotorIdentifier.RIGHT.value, -speed, angle))
+            self._com.send("MOVB;{};{};{}".format(
+                MotorIdentifier.LEFT.value, speed, angle))
+            self._com.send("MOVB;{};{};{}".format(
+                MotorIdentifier.RIGHT.value, speed, -angle))
+            if self._result_listener is not None:
+                self._result_listener({"name": "turn_sharp", "direction": "right", "speed": speed, "angle": angle})
         else:
-            raise UnknownDirectionError("Direction must be SHARP_LEFT or SHARP_RIGHT. Direction =", direction)
+            raise UnknownDirectionError(
+                "Direction must be SHARP_LEFT or SHARP_RIGHT. Direction =", direction)
 
-    def advance_cm(self, distance, speed=200, circumference=20.42):
+    def advance_cm(self, distance, speed=200, circumference=20.1):
         if distance > 0:
             self.move_by(MotorIdentifier.BOTH, speed, distance/circumference)
         elif distance < 0:
@@ -66,8 +92,8 @@ class UnknownDirectionError(MotorError):
         self.expression = expression
         self.message = message
 
+
 class InvalidIdentifierError(MotorError):
     def __init__(self, message="", expression=None):
         self.expression = expression
         self.message = message
-
