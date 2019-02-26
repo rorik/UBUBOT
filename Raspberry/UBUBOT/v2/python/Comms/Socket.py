552be1eaa8f5a53d7506d0efeb1f16241d1bab06
Roderick Diaz <rdg1003@alu.ubu.es>
2019-02-26 21:03:22 +0100

    Added socket communication #9

---
+++ Raspberry/UBUBOT/v2/python/Comms/Socket.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import socketio
+import json
+import atexit
+
+
+class SocketCommunication:
+    _connection = None
+
+    def __init__(self):
+        if SocketCommunication._connection is None:
+            SocketCommunication._connection = socketio.Client()
+            SocketCommunication._connection.connect('http://localhost:80')
+            atexit.register(SocketCommunication.disconnect)
+
+    def send(self, label, data):
+        SocketCommunication._connection.emit(label, data)
+
+    def send_json(self, label, data):
+        self.send(label, json.dumps(data))
+
+    @staticmethod
+    def disconnect():
+        if SocketCommunication._connection is not None:
+            SocketCommunication._connection.disconnect()
+            SocketCommunication._connection = None
